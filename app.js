0cf8395bef3be6c0839eb7554ae19ae29a8b6128
eslam-mah <eslamxxxxx517@gmail.com>
2026-07-08 11:41:22 -0700

    Added structure files

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+// Middleware to parse incoming JSON payloads
+app.use(express.json());
+
+const patientRoutes = require('./src/routes/patientRoutes');
+
+app.use('/api/patients', patientRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Node.js Training API is running smoothly.');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
---
+++ src/controllers/patientController.js
@@ -0,0 +1,24 @@
+const db = require('../config/db');
+exports.getAllPatients = async (req, res) => {
+    try {
+        const [rows] = await db.query('SELECT * FROM patients');
+        res.status(200).json(rows);
+    } catch (error) {
+        res.status(500).json({ success: false, error: error.message });
+    }
+};
+
+exports.getPatientByMrn = async (req, res) => {
+    const { mrn } = req.params;
+    try {
+        const [rows] = await db.query('SELECT * FROM patients WHERE mrn = ?', [mrn]);
+        
+        if (rows.length === 0) {
+            return res.status(404).json({ success: false, message: "Patient not found" });
+        }
+        
+        res.status(200).json(rows[0]);
+    } catch (error) {
+        res.status(500).json({ success: false, error: error.message });
+    }
+};
